c74a984eb7da19fd4c5b87f35e3510837cc4b93d
Leanne Shapton <shaptonleanne@gmail.com>
2014-08-19 21:21:02 -0400

    added only show to not_attending and attending routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   get 'home' => 'home#index'
 
-  resources :not_attendings
+  resources :not_attendings, only: :show
 
-  resources :attendings
+  resources :attendings, only: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

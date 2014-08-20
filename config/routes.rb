a71b3db631294fbaf2a2e7e49256e754f4210c5a
Leanne Shapton <shaptonleanne@gmail.com>
2014-08-19 21:34:18 -0400

    reverted back to old routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get 'home' => 'home#index'
+  
+  resources :not_attendings
 
-  get "not_attendings/show" => 'not_attendings#index'
-
-  get "attendings/show" => 'attendings#index'
-
-  get "not_attendings/show" => 'not_attendings#show'
-
-  get "attendings/show" => 'attendings#show'
+  resources :attendings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

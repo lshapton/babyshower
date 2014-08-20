41c736b587d67be62dc0a2f85000915f46cd1f25
Leanne Shapton <shaptonleanne@gmail.com>
2014-08-20 19:59:48 -0400

    added user signin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+
+admin = Admin.create!(:email => "shaptonleanne@gmail.com", :password => 'october2014')
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +9,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
